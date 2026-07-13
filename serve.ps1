333047181b1a924e21fff05f83bf9b2409064e1c
Diego Santos <diegodelossantos.1224@gmail.com>
2026-07-13 17:44:11 -0600

    Replace Shoot & Create process photo with client-provided image

--- serve.ps1
+++ serve.ps1
@@ -8,7 +8,7 @@ Write-Host "Serving $root on http://localhost:$port/"
 $mime = @{
   ".html" = "text/html"; ".css" = "text/css"; ".js" = "application/javascript";
   ".json" = "application/json"; ".png" = "image/png"; ".jpg" = "image/jpeg";
-  ".svg" = "image/svg+xml"; ".ico" = "image/x-icon"
+  ".svg" = "image/svg+xml"; ".ico" = "image/x-icon"; ".avif" = "image/avif"; ".webp" = "image/webp"
 }
 
 while ($listener.IsListening) {
